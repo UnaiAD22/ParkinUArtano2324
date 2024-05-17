2799edd830bc5811710576b8219d79561f370ecd
UnaiAD22 <101432395+UnaiAD22@users.noreply.github.com>
2024-05-17 19:59:37 +0200

    Cambio color + Completar Registro + Terminar funciones Usuario

--- app/src/main/java/com/lksnext/parkinguartano/domain/Usuario.java
+++ app/src/main/java/com/lksnext/parkinguartano/domain/Usuario.java
@@ -20,4 +20,16 @@ public class Usuario {
     public String getPassword(){
         return this.password;
     }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
 }
